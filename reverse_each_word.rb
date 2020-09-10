6fc15cc0f1f5a1f394c60e7ee5595993ca5523b8
Brock Byrd <brockbyrdd@gmail.com>
2020-09-10 23:37:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   return_array.join(" ")
 end
+
+def_reverse_each_word_with_collect(string)
+  array = string.split(" ")
+  return_array = []
+  array.collect do |string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
